db149be163fae38f553b8da8934ba7f125794554
sebenchohra <sbenchohra12@gmail.com>
2014-09-10 11:30:25 -0700

    Added first name to index.html

--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,19 +14,42 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+int startX, startY, endX, endY, r;
+
 public void setup()
 {
-  size(300,300);
-  noLoop();
+
+	size(300,300);
+	startX = 0;
+	startY = 150;
+	endX = 0;
+	endY = 150;
+	r = (int)(Math.random()*256);
+	strokeWeight(5);
+	background(0);
+	noLoop();
 }
 public void draw()
 {
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
+	stroke((int)(Math.random()*256), (int)(Math.random()*256),(int)(Math.random()*256));
+	while (endX < 300) 
+	{
+		endX = startX + (int)(Math.random()*11);
+		endY = startY + (int)(Math.random()*11) - 5;
+		line(startX,startY,endX,endY);
+		startX = endX;
+		startY = endY;
+		
+	}	
+
 }
 public void mousePressed()
 {
-  redraw();
+	startX = 0;
+	startY = 150;
+	endX = 0;
+	endY = 150;
+	redraw();
 }
 
   static public void main(String[] passedArgs) {
